4712668f19e852ee959b2bd1ba6b2baaffaa1392
OsmanBytyqi <obytyqi2001@gmail.com>
2022-08-18 17:13:43 +0200

    new config for vim

---
+++ nvim/.config/nvim/lua/osmani/init.lua
@@ -0,0 +1,39 @@
+require("osmani.packer")
+require("osmani.keymap")
+require("osmani.set")
+require("osmani.statusline")
+
+local augroup = vim.api.nvim_create_augroup
+Osmani = augroup('osmani', {})
+
+local autocmd = vim.api.nvim_create_autocmd
+local yank_group = augroup('HighlightYank', {})
+
+autocmd('TextYankPost', {
+    group = yank_group,
+    pattern = '*',
+    callback = function()
+        vim.highlight.on_yank({
+            higroup = 'IncSearch',
+            timeout = 40,
+        })
+    end,
+})
+
+autocmd({"BufEnter", "BufWinEnter", "TabEnter"}, {
+    group = Osmani,
+    pattern = "*.rs",
+    callback = function()
+        require("lsp_extensions").inlay_hints{}
+    end
+})
+
+autocmd({"BufWritePre"}, {
+    group =Osmani,
+    pattern = "*",
+    command = "%s/\\s\\+$//e",
+})
+
+vim.g.netrw_browse_split = 0
+vim.g.netrw_banner = 0
+vim.g.netrw_winsize = 25
