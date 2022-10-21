aafda779ca7dc962397595446d700750f8248bad
OsmanBytyqi <obytyqi2001@gmail.com>
2022-10-21 22:14:22 +0200

    fix treesitter

--- nvim/.config/nvim/after/plugin/lsp.lua
+++ nvim/.config/nvim/after/plugin/lsp.lua
@@ -2,8 +2,9 @@ local Remap = require("osmani.keymap")
 local nnoremap = Remap.nnoremap
 local inoremap = Remap.inoremap
 
---local sumneko_root_path = "/home/mpaulson/personal/sumneko"
---local sumneko_binary = sumneko_root_path .. "/bin/lua-language-server"
+local sumneko_root_path = "/home/osmani/.repos/lua-language-server"
+
+local sumneko_binary = sumneko_root_path .. "/usr/bin/lua-language-server"
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities.textDocument.completion.completionItem.snippetSupport = true
@@ -149,30 +150,30 @@ require("lspconfig").rust_analyzer.setup(config({
     --]]
 }))
 
---require("lspconfig").sumneko_lua.setup(config({
---	cmd = { sumneko_binary, "-E", sumneko_root_path .. "/main.lua" },
---	settings = {
---		Lua = {
---			runtime = {
---				-- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
---				version = "LuaJIT",
---				-- Setup your lua path
---				path = vim.split(package.path, ";"),
---			},
---			diagnostics = {
---				-- Get the language server to recognize the `vim` global
---				globals = { "vim" },
---			},
---			workspace = {
---				-- Make the server aware of Neovim runtime files
---				library = {
---					[vim.fn.expand("$VIMRUNTIME/lua")] = true,
---					[vim.fn.expand("$VIMRUNTIME/lua/vim/lsp")] = true,
---				},
---			},
---		},
---	},
---}))
+require("lspconfig").sumneko_lua.setup(config({
+	cmd = { sumneko_binary, "-E", sumneko_root_path .. "/main.lua" },
+	settings = {
+		Lua = {
+			runtime = {
+				-- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+				version = "LuaJIT",
+				-- Setup your lua path
+				path = vim.split(package.path, ";"),
+			},
+			diagnostics = {
+				-- Get the language server to recognize the `vim` global
+				globals = { "vim" },
+			},
+			workspace = {
+				-- Make the server aware of Neovim runtime files
+				library = {
+					[vim.fn.expand("$VIMRUNTIME/lua")] = true,
+					[vim.fn.expand("$VIMRUNTIME/lua/vim/lsp")] = true,
+				},
+			},
+		},
+	},
+}))
 
 local opts = {
 	-- whether to highlight the currently hovered symbol
